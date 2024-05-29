0ff7ed944f1ba7f1667b9967f17a5bf59e821a01
claudinei <claudineigaiteiro@gmail.com>
2024-05-28 22:18:38 -0300

    #CRM- Aula 1 a 4.

---
+++ Python/CFBCursos/Aula02/aula02.py
@@ -0,0 +1,16 @@
+#Comentário
+
+"""
+Comentário de multiplas linhas
+"""
+
+canal="CFB Cursos"
+curso="Curso de Python"
+num1=num2=res=0
+
+print(canal + " - " + curso)
+
+if 10 > 20:
+    print("Maior")
+    print("aula 2")
+print("FIM")
---
+++ Python/CFBCursos/Aula03/aula03.py
@@ -0,0 +1,11 @@
+num1=num2=res=0
+
+def cn():
+    global canal
+    canal = "CFB Cursos"
+    teste="FUNE"
+
+cn()
+    
+print(canal)
+#print(teste)
